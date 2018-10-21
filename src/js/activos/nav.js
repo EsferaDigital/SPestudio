038bc32f2cf0cab746e1eb029e0c8d6710de1ce6
Gabriel Zavando <gabrielzavando@gmail.com>
2018-10-21 19:32:46 -0300

    primer Commit

---
+++ src/js/activos/nav.js
@@ -0,0 +1,23 @@
+let mainNav = document.getElementById('mainNav');
+let openMenu = document.getElementById('openMenu');
+let closeMenu = document.getElementById('closeMenu');
+
+function hideMenu(){
+  mainNav.classList.remove('show-menu');
+}
+
+function showMenu(){
+  mainNav.classList.toggle('show-menu');
+}
+
+// Ejecutamos las funciones
+
+document.addEventListener('click', e => {
+  if(e.target !== mainNav && e.target !== toggleMenu) hideMenu()
+});
+
+mainNav.addEventListener('mouseleave', hideMenu);
+openMenu.addEventListener('click', showMenu);
+closeMenu.addEventListener('click', hideMenu);
+
+
