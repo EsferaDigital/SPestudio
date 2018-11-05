1a3cab8148b2abe5673cd4298fb5aca68c1fef55
Gabriel Zavando <gabrielzavando@gmail.com>
2018-11-05 12:24:24 -0300

    Separé los js por página y termine el contenido de nuestro estudio

---
+++ src/js/dev/modal.js
@@ -0,0 +1,12 @@
+const open = document.getElementById('open-modal');
+const modal = document.getElementById('modal');
+const close = document.getElementById('close-modal');
+
+open.addEventListener('click', () => {
+  modal.classList.toggle('zoom-in');
+  modal.classList.remove('zoom-out');
+});
+
+close.addEventListener('click', () => {
+  modal.classList.replace('zoom-in', 'zoom-out');
+});
